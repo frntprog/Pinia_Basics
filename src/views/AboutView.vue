04257037880b4f555e288cc9d8fcf4542af0b109
frntprog <frntprog@gmail.com>
2023-09-25 17:44:55 +0300

    pinia introduction

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>{{ count }}</p>
+  </div>
+</template>
+
+<script setup>
+import { storeToRefs } from "pinia";
+import { useCounterStore } from "../stores/counter";
+
+const store = useCounterStore();
+const { count } = storeToRefs(store);
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <div class="count">{{ count }}</div>
+  </div>
+  <div class="buttons">
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <hr />
+    <p>This number is doubled: {{ doubleCount }}</p>
+    <hr />
+    <input type="text" v-model="count" />
+  </div>
+</template>
+
+<script setup>
+import { storeToRefs } from "pinia";
+import { useCounterStore } from "../stores/counter";
+
+const store = useCounterStore();
+const { count, doubleCount } = storeToRefs(store);
+
+const { increment, decrement } = store;
+</script>
+
+<style scoped>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
